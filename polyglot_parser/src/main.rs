5a7975b8bc82d7b319d18fa64c795b7aeede70a6
LyonSyonII <slimystudios@protonmail.com>
2022-02-10 17:13:21 +0100

    Refactored code and added errors

---
+++ polyglot_parser/src/main.rs
@@ -0,0 +1,287 @@
+use std::rc::Rc;
+use clap::Parser as P;
+use inner::inner;
+use pest::iterators::{Pair, Pairs};
+use pest::Parser as Pest;
+use serde::{Deserialize, Serialize, Serializer};
+
+mod tree;
+use show_my_errors::{AnnotationList, Stylesheet};
+use tree::*;
+
+// TODO! Check all values on list and dictionary too see if all have the same type
+
+#[derive(clap::Parser)]
+#[clap(version, about)]
+struct Cli {
+    file: std::path::PathBuf,
+    #[clap(short, long)]
+    debug: bool
+}
+
+#[derive(pest_derive::Parser, pest_typed_tree::TypedTree, Serialize, Deserialize, Debug)]
+#[grammar = "grammar.pest"]
+struct Parser;
+
+fn main() {
+    parse();
+}
+
+fn parse() {
+    let cli = Cli::parse();
+    let mut global = Scope::new(vec![]);
+    global.set_file(cli.file.clone(), std::fs::read_to_string(&cli.file).unwrap());
+    
+    let file = global.file().to_owned();
+    let root = nodes::Main::new(Parser::parse(Rule::Main, &file).unwrap().next().unwrap());
+    
+    let mut main = Main::new(Vec::new());
+    for expr in root.list_Expr() {
+        global.change_ctx(expr.text().into());
+        main.0.push(parse_expr(expr, &mut global));
+    }
+    
+    if global.err_found() && !cli.debug {
+        return;
+    }
+
+    let buffer = serde_yaml::to_string(&main).unwrap();
+    if cli.debug { println!("{buffer}") }
+    let path = global.file_name().with_extension("yml");
+    std::fs::write(path, buffer).unwrap();
+}
+
+fn parse_expr(expr: nodes::Expr, scope: &mut Scope) -> Expr {
+    match expr.to_enum() {
+        nodes::ExprChildren::Init(init) => Expr::Init(Box::new(parse_init(init, scope))),
+        nodes::ExprChildren::Decl(_) => todo!(),
+        nodes::ExprChildren::Typedef(_) => todo!(),
+        nodes::ExprChildren::Assig(_) => todo!(),
+        nodes::ExprChildren::ListRemAssig(_) => todo!(),
+        nodes::ExprChildren::AddAssig(_) => todo!(),
+        nodes::ExprChildren::SubAssig(_) => todo!(),
+        nodes::ExprChildren::MulAssig(_) => todo!(),
+        nodes::ExprChildren::DivAssig(_) => todo!(),
+        nodes::ExprChildren::ModAssig(_) => todo!(),
+        nodes::ExprChildren::PowAssig(_) => todo!(),
+    }
+}
+
+fn parse_init(init: nodes::Init, scope: &mut Scope) -> Init {
+    let name = init.get_Name().text().to_owned();
+    let node_v = init.get_Value();
+    let parsed_v = parse_value(&node_v, scope);
+    
+    let ty = match init.get_Type() {
+        // Check if variable is of the same type as assignment
+        Some(node_t) => {
+            let t = parse_type(&node_t);
+            let vt = parse_type_from_value(&parsed_v, scope);
+            if t == vt {
+                t
+            } else {
+                printerr(&node_v.range(), "wrong assignment type", &format!("expected '{t}', found '{vt}'"), scope);
+                Type::Err
+            }
+        }
+        // No explicit type annotation, inferring type from value
+        None => parse_type_from_value(&parsed_v, scope),
+    };
+
+    scope.insert(name.clone(), ty.clone());
+    Init::new(name, ty, parsed_v, scope.ctx().into())
+}
+
+fn parse_value(value: &nodes::Value, scope: &mut Scope) -> Value {
+    match value.to_enum() {
+        nodes::ValueChildren::Int(i) => Value::Int(i.text().parse().unwrap()),
+        nodes::ValueChildren::Num(n) => Value::Num(n.text().parse().unwrap()),
+        nodes::ValueChildren::Bool(b) => match b.text() {
+            "true" => Value::Bool(Bool::Primitive(true)),
+            "false" => Value::Bool(Bool::Primitive(false)),
+            _ => Value::Bool(Bool::Cmp(parse_cmp(b.list_Cmp().next().unwrap()))),
+        },
+        nodes::ValueChildren::Char(c) => Value::Char(c.text().as_bytes()[0] as char),
+        nodes::ValueChildren::Str(s) => Value::Str(
+            s.text()
+                .strip_prefix('"')
+                .unwrap()
+                .strip_suffix('"')
+                .unwrap()
+                .into(),
+        ),
+        nodes::ValueChildren::Tuple(t) => Value::Tuple(
+            t.list_Value()
+                .map(|val| parse_value(&val, scope))
+                .collect::<Vec<Value>>(),
+        ),
+        nodes::ValueChildren::Struct(s) => Value::Struct ({
+            s.list_StructVal().map(|sval| {(
+                sval.get_Name().text().to_owned(),
+                parse_value(&sval.get_Value(), scope)
+            )}).collect()
+        }),
+        nodes::ValueChildren::TupleAccess(ta) => Value::TupleAccess {
+            name: ta.get_Name().text().into(),
+            access_type: match ta.get_TupleAccessType().to_enum() {
+                nodes::TupleAccessTypeChildren::Name(n) => TupleAccessType::Member(n.text().into()),
+                nodes::TupleAccessTypeChildren::Index(i) => {
+                    TupleAccessType::Index(i.text().parse().unwrap())
+                }
+            },
+            name_range: ta.get_Name().range(),
+            access_range: ta.get_TupleAccessType().range(),
+        },
+        nodes::ValueChildren::List(l) => Value::List(
+            l.list_Value()
+                .map(|val| parse_value(&val, scope))
+                .collect::<Vec<Value>>(),
+        ),
+        // TODO! Check if list is a List or a Dict and access it properly
+        nodes::ValueChildren::ListAccess(_) => todo!(),
+        nodes::ValueChildren::Dict(d) => Value::Dict(
+            d.list_DictPair()
+                .map(|pair| {
+                    (
+                        parse_value(&pair.get_first_Value(), scope),
+                        parse_value(&pair.get_second_Value(), scope),
+                    )
+                })
+                .collect::<Vec<(Value, Value)>>(),
+        ),
+        nodes::ValueChildren::Name(n) => Value::Var { name: n.text().into(), range: n.range() },
+
+        // TODO! Complex values
+
+        // TODO! Check if both values are of the same type
+        nodes::ValueChildren::Op(_) => todo!(),
+        nodes::ValueChildren::RetExpr(_) => todo!(),
+        nodes::ValueChildren::Call(_) => todo!(),
+    }
+}
+
+fn parse_type(ty: &nodes::Type) -> Type {
+    match ty.to_enum() {
+        nodes::TypeChildren::TInt(_) => Type::Int,
+        nodes::TypeChildren::TNum(_) => Type::Num,
+        nodes::TypeChildren::TBool(_) => Type::Bool,
+        nodes::TypeChildren::TChar(_) => Type::Char,
+        nodes::TypeChildren::TStr(_) => Type::Str,
+        nodes::TypeChildren::TTuple(t) => Type::Tuple(
+            t.list_Type().map(|t| parse_type(&t)).collect::<Vec<Type>>(),
+        ),
+        nodes::TypeChildren::TStruct(s) => Type::Struct(
+            s.list_StructMem().map(|mem| (
+                mem.get_Name().text().into(), parse_type(&mem.get_Type())
+            )).collect()
+        ),
+        nodes::TypeChildren::TList(l) => Type::List(Box::new(parse_type(&l.get_Type()))),
+
+        nodes::TypeChildren::TDict(d) => Type::Dict(Box::new((
+            parse_type(&d.get_first_Type()),
+            parse_type(&d.get_second_Type()),
+        ))),
+
+        // On typedef add type to variables, then check its value and return it
+        nodes::TypeChildren::TCustom(c) => Type::Custom(c.text().into()),
+    }
+}
+
+fn parse_type_from_value(value: &Value, scope: &mut Scope) -> Type {
+    match value {
+        Value::Int(_) => Type::Int,
+        Value::Num(_) => Type::Num,
+        Value::Bool(_) => Type::Bool,
+        Value::Char(_) => Type::Char,
+        Value::Str(_) => Type::Str,
+        Value::Tuple(t) => Type::Tuple(
+            t
+                .iter()
+                .map(|val| parse_type_from_value(val, scope))
+                .collect::<Vec<Type>>(),
+        ),
+        Value::Struct(members) => Type::Struct(
+            members
+                .into_iter()
+                .map(|mem| (mem.0.clone(), parse_type_from_value(&mem.1, scope)))
+                .collect()
+        ),
+        Value::List(l) => Type::List(Box::new(parse_type_from_value(&l[0], scope))),
+        Value::Dict(d) => Type::Dict(Box::new((
+            parse_type_from_value(&d[0].0, scope),
+            parse_type_from_value(&d[0].1, scope),
+        ))),
+        Value::Var {name, range} => {
+            if let Some(var_t) = scope.get(name).cloned() {
+                var_t
+            } else {
+                printerr(range, &format!("variable '{name}' does not exist"), "not declared", scope);
+                Type::Err
+            }
+        }
+        Value::TupleAccess { name, access_type, name_range, access_range} => {
+            let tuple_type = scope.get(name).unwrap_or(&Type::Err).clone();
+            if tuple_type == Type::Err {
+                printerr(name_range, "accessed invalid tuple/struct", "struct does not exist", scope);
+                return tuple_type
+            }
+            
+            match access_type {
+                TupleAccessType::Member(member) => {
+                    let mut struct_t = inner!(tuple_type, if Type::Struct, else { return printerr(access_range, "accessed tuple by member name", format!("use index instead: {name}.0"), scope).type_err() });
+                    struct_t.sort_unstable();
+                    if let Ok(ty) = struct_t.binary_search_by_key(&member, |(a, _)| a) {
+                        struct_t[ty].1.clone()
+                    } else {
+                        printerr(access_range, format!("member '{name}.{member}' does not exist"), "not declared", scope);
+                        Type::Err
+                    }
+                }
+                TupleAccessType::Index(index) => {
+                    let tuple_t = inner!(tuple_type, if Type::Tuple, else { return printerr(access_range, "accessed struct by index", format!("use member name instead: {name}.member"), scope).type_err()});
+                    if let Some(ty) = tuple_t.get(*index) {
+                        ty.clone()
+                    } else {
+                        let n_elems = tuple_t.len();
+                        let n_elems = if n_elems > 1 {
+                            format!("{} elements", n_elems)
+                        } else {
+                            "1 element".into()
+                        };
+                        
+                        printerr(&(name_range.start..access_range.end), "index out of bounds", format!("tuple has {}, trying to access element number {}", n_elems, index+1), scope).type_err()
+                    }
+                }
+            }
+        }
+        Value::ListAccess { name, access_type, range } => {
+            todo!()
+        }
+        // TODO! Complex values
+        Value::Op(_) => todo!(),
+        Value::Call{ name, args} => todo!(),
+        Value::RetExpr(_) => todo!(),
+        Value::Err => Type::Err
+    }
+}
+
+fn parse_cmp(cmp: nodes::Cmp) -> Cmp {
+    todo!()
+}
+
+
+struct PrintErr;
+impl PrintErr {
+    pub fn type_err(self) -> Type { Type::Err }
+    
+    pub fn value_err(self) -> Value { Value::Err }
+}
+
+fn printerr(range: &std::ops::Range<usize>, header: impl AsRef<str>, text: impl AsRef<str>, scope: &mut Scope) -> PrintErr {
+    let mut list = AnnotationList::new(scope.file_name().to_string_lossy(), scope.file());
+    list.error(range.start..range.end, header.as_ref(), text.as_ref()).unwrap();
+    list.show_stderr(&Stylesheet::colored()).unwrap();
+    println!();
+    scope.err();
+    PrintErr
+}
---
+++ polyglot_parser/src/tree.rs
@@ -0,0 +1,259 @@
+use std::{ops::{Add, Range}, path::PathBuf};
+
+
+use derive_new::new;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, new)]
+pub struct Main(pub Vec<Expr>);
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Expr {
+    Init(Box<Init>),
+    Decl,
+    Assig,
+    Typedef,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum RetExpr {}
+
+#[derive(Debug, Serialize, Deserialize, new)]
+pub struct Init {
+    pub name: String,
+    pub r#type: Type,
+    pub value: Value,
+    //pub line_num: usize,
+    pub context: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, new)]
+pub enum Value {
+    Int(i64),
+    Num(f64),
+    Bool(Bool),
+    Char(char),
+    Str(String),
+    Tuple(Vec<Value>),
+    Struct(Vec<(String, Value)>),
+    TupleAccess {
+        name: String,
+        access_type: TupleAccessType,
+        name_range: Range<usize>,
+        access_range: Range<usize>,
+    },
+    List(Vec<Value>),
+    ListAccess {
+        name: String,
+        access_type: ListAccessType,
+        range: Range<usize>
+    },
+    Dict(Vec<(Value, Value)>),
+    Var {
+        name: String,
+        range: Range<usize>,
+    },
+    Op(Op),
+    Call {
+        name: String,
+        args: Vec<String>,
+    },
+    RetExpr(RetExpr),
+    Err
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Bool {
+    Primitive(bool),
+    Cmp(Cmp),
+}
+
+#[derive(Debug, Serialize, Deserialize, new)]
+pub struct Call {
+    pub name: String,
+    pub args: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum TupleAccessType {
+    Member(String),
+    Index(usize),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ListAccessType {
+    List(usize),
+    Dict(Box<Value>),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Op {}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Cmp {}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialOrd, Eq, Ord)]
+pub enum Type {
+    Int,
+    Num,
+    Bool,
+    Char,
+    Str,
+    Tuple(Vec<Type>),
+    Struct(Vec<(String, Type)>),
+    List(Box<Type>),
+    Dict(Box<(Type, Type)>),
+    Custom(String),
+    Err,
+}
+
+// CUSTOM DEFINED
+pub struct Scope {
+    vars: std::collections::HashMap<String, Type>,
+    ctx: String,
+    file: (String, PathBuf),
+    err: bool
+}
+
+// IMPLS
+impl PartialEq for Type {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Tuple(l0), Self::Tuple(r0)) => l0 == r0,
+            (Self::Struct(l0), Self::Struct(r0)) => l0 == r0,
+            (Self::Custom(l0), Self::Custom(r0)) => l0 == r0,
+            (Self::Struct(l0), Self::Tuple(r0)) => {
+                for (l0, r0) in l0.into_iter().zip(r0) {
+                    if l0.1 != *r0 {
+                        return false;
+                    }
+                }
+                true
+            }
+            (Type::Int, Type::Num) | (Type::Num, Type::Int) => true,
+            _ => core::mem::discriminant(self) == core::mem::discriminant(other),
+        }
+    }
+}
+
+impl std::fmt::Display for Type {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Type::Int => write!(f, "int"),
+            Type::Num => write!(f, "num"),
+            Type::Bool => write!(f, "bool"),
+            Type::Char => write!(f, "char"),
+            Type::Str => write!(f, "str"),
+            Type::Tuple(t) => todo!(),
+            Type::Struct(_) => todo!(),
+            Type::List(l) => write!(f, "[{l}]"),
+            Type::Dict(d) => write!(f, "[{} -> {}]", d.0, d.1),
+            Type::Custom(c) => write!(f, "{c}"),
+            Type::Err => unreachable!()
+        }
+    }
+}
+
+impl Scope {
+    pub fn new(current_scope: Vec<(String, Type)>) -> Scope {
+        let mut map = std::collections::HashMap::with_capacity(current_scope.len());
+        for var in current_scope.into_iter() {
+            map.insert(var.0, var.1);
+        }
+        
+        Scope {
+            vars: map,
+            ctx: String::new(),
+            file: (String::new(), PathBuf::new()),
+            err: false,
+        }
+    }
+    
+    pub fn change_ctx(&mut self, new: String) {
+        self.ctx = new;
+    }
+
+    pub fn ctx(&self) -> &str {
+        self.ctx.as_str()
+    }
+    
+    pub fn set_file(&mut self, name: PathBuf, contents: String) {
+        self.file = (contents, name);
+    }
+
+    pub fn file(&self) -> &str {
+        self.file.0.as_str()
+    }
+
+    pub fn file_name(&self) -> &std::path::Path {
+        self.file.1.as_path()
+    }
+
+    pub fn err(&mut self) {
+        self.err = true;
+    }
+    
+    pub fn err_found(&self) -> bool {
+        self.err
+    }
+}
+
+impl std::ops::Deref for Scope {
+    type Target = std::collections::HashMap<String, Type>;
+    
+    fn deref(&self) -> &Self::Target {
+        &self.vars
+    }
+}
+
+impl std::ops::DerefMut for Scope {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.vars
+    }
+}
+
+pub trait GetRange {
+    fn range(&self) -> std::ops::Range<usize>;
+}
+
+impl GetRange for crate::nodes::Value<'_> {
+    fn range(&self) -> std::ops::Range<usize> {
+        self.span().start()..self.span().end()
+    }
+}
+
+impl GetRange for crate::nodes::Type<'_> {
+    fn range(&self) -> std::ops::Range<usize> {
+        self.span().start()..self.span().end()
+    }
+}
+
+impl GetRange for crate::nodes::Name<'_> {
+    fn range(&self) -> std::ops::Range<usize> {
+        self.span().start()..self.span().end()
+    }
+}
+
+impl GetRange for crate::nodes::TupleAccess<'_> {
+    fn range(&self) -> std::ops::Range<usize> {
+        self.span().start()..self.span().end()
+    }
+}
+
+impl GetRange for crate::nodes::TupleAccessType<'_> {
+    fn range(&self) -> std::ops::Range<usize> {
+        self.span().start()-1..self.span().end()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
